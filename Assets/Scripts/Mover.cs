9c1acc82fc4b4c43f612d6e8b38ac8ba48ee3256
Kutay Can Kaynak <56002368+kutagorn@users.noreply.github.com>
2021-11-12 18:52:06 +0300

    first

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+ 
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        var normalisedSpeed = speed * Time.deltaTime;
+        var xValue = Input.GetAxis("Horizontal") * normalisedSpeed;
+        var zValue = Input.GetAxis("Vertical") * normalisedSpeed;
+        transform.Translate(xValue, 0, zValue);
+    }
+}
